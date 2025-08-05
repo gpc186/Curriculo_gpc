5f8dee4e9b9851c159a5bf74b0fa47ab5f4f4a9a
gpc186 <gpc186@gmail.com>
2025-08-05 15:43:31 -0300

    Testes dos botões

---
+++ main.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll(".info_buttons_cv");
+
+const textBody = document.querySelector(".info_buttons_body_cv");
+
+function hoverBoxText(){
+
+	buttons.forEach(function(button) {
+
+		button.addEventListener('click', function(){
+
+			button.classList.toggle("visivel_box");
+			bodyText.classList.toggle("visivel_body")
+
+		});
+
+	});
+
+};
+
+hoverBoxText()
