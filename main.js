dcf3f8d843b7c584faa03982083573fc80381eb7
gpc186 <gpc186@gmail.com>
2025-08-08 10:02:25 -0300

    Preparando a implementação do JS para hover

--- main.js
+++ main.js
@@ -19,6 +19,26 @@ function clickBoxText(){
 
 clickBoxText();
 
+const hiddenItemLeft = document.querySelector(".projects_item_hidden_left_pf");
+const hiddenItemRight = document.querySelector(".projects_item_hidden_right_pf");
+
+function showHiddenEffect() {
+	
+	hiddenItemLeft.classList.remove("rojects-hover-hidden-item");
+	hiddenItemLeft.classList.add("projects-hover-showing-item");
+
+	hiddenItemRight.classList.remove("rojects-hover-hidden-item")
+	hiddenItemRight.classList.add("projects-hover-showing-item")
+}
+
+function hideHiddenEffect() {
+	hiddenItemLeft.classList.remove("projects-hover-showing-item");
+	hiddenItemLeft.classList.add("rojects-hover-hidden-item");
+
+	hiddenItemRight.classList.remove("projects-hover-showing-item");
+	hiddenItemRight.classList.add("rojects-hover-hidden-item");
+}
+
 const projectItem = document.querySelectorAll(".projects_item_pf");
 
 function hoverBoxProject(){
