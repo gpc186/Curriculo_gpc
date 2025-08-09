d3911b78831faf4f087adaca737c45385b4be45a
gpc186 <gpc186@gmail.com>
2025-08-08 22:07:26 -0300

    Mais alterações e testes no JS

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ function showHiddenEffect(projectItemHover) {
 
 	hiddenItemLeft.classList.remove("projects-hover-hidden-item");
 	hiddenItemLeft.classList.add("projects-hover-showing-item");
-	hiddenItemRight.classList.remove("projects-hover-hidden-item")
-	hiddenItemRight.classList.add("projects-hover-showing-item")
+	hiddenItemRight.classList.remove("projects-hover-hidden-item");
+	hiddenItemRight.classList.add("projects-hover-showing-item");
 }
 
 function hideHiddenEffect(projectItemHover) {
 	const hiddenItemLeft = projectItemHover.querySelector(".projects_item_hidden_left_pf");
-	const hiddenItemRight = projectItemHover.querySelector(".projects_item_hidden_right_pf");
+	const hiddenItemRight = projectItemHover.querySelector(".projects_item_hidden_right_pf");	
 
 	hiddenItemLeft.classList.remove("projects-hover-showing-item");
 	hiddenItemLeft.classList.add("projects-hover-hidden-item");
@@ -32,21 +32,33 @@ function hideHiddenEffect(projectItemHover) {
 	hiddenItemRight.classList.add("projects-hover-hidden-item");
 }
 
-const projectItems = document.querySelectorAll(".projects_item_pf");
+function expandHoverBox(projectItemHover) {
+	projectItemHover.classList.toggle("projects-hover-box");
+	showHiddenEffect(projectItemHover);
+}
 
+function retractHoverBox(projectItemHover) {
+	projectItemHover.classList.toggle("projects-hover-box");
+	hideHiddenEffect(projectItemHover);
+}
+
+const projectItems = document.querySelectorAll(".projects_item_pf");
 
 projectItems.forEach(function(projectItem){
 	projectItem.addEventListener("mouseenter", function(){
-		const projectItemCurrentHover = this
+		const projectItemCurrentHover = this;
 		projectItems.forEach(function(projectItemHover) {
 			if (projectItemHover === projectItemCurrentHover) {
-				showHiddenEffect(projectItemHover)
+				expandHoverBox(projectItemHover);
 			} else {
-				hideHiddenEffect(projectItemHover)
+				retractHoverBox(projectItemHover);
 			}
-		})
-	}) 
-})
-
-
-
+		});
+	
+		projectItem.addEventListener("mouseleave", function(){
+			projectItems.forEach(function(projectItemHover){
+				retractHoverBox(projectItemHover);
+			});
+		});
+	});
+});
